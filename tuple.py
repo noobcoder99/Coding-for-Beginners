c311e6d6de5f71d4b8c4f891497d002e4fe01a60
fmc <fmc@skynet3.local>
2018-05-01 17:53:52 +1000

    added_new

---
+++ tuple.py
@@ -0,0 +1,8 @@
+days = ('Mon','Tue','Wed','Thu','Fri')
+print('days:',type(days))
+print('Days of the week:',days)
+print('No. of days in week:',len(days))
+print('Start of the week:',days[0])
+user = ('John','Doe', 'Paris', '555-1234')
+print('Name:', user[0],user[1])
+print('Phone:',user[3])
